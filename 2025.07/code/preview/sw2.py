b2f6e303b3e9179e5f9b25c878482f4b88cb0501
Migong0311 <kimms000311@gmail.com>
2025-07-20 17:22:45 +0900

    파일 이동 커밋

--- 2025.07/code/preview/sw2.py
+++
@@ -1,9 +0,0 @@
-a=int(input())
-if a < 1 or a > 9999:
-    print("Input out of range")
-else:
-    total = 0
-    while a > 0:
-        total += a % 10   # 일의 자리 더하기
-        a //= 10          # 한 자리씩 줄이기
-    print(total)
