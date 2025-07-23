57a516d2e8aa3da1082b7ed955b441ac534ce3d4
Migong0311 <kimms000311@gmail.com>
2025-07-23 23:57:59 +0900

    feat : 1684.대규모_도서_대여_서비스_Lv5 문제 추가

---
+++ 2025.07/code/실습&과제/3week/gltlab/3주차_고난도_문제/1684.대규모_도서_대여_서비스_Lv5.py
@@ -0,0 +1,16 @@
+number_of_people = 0
+
+def decrease_book():
+    pass
+
+def create_user():
+    pass
+
+def rental_book(info):
+    pass
+
+name = ['김시습', '허균', '남영로', '임제', '박지원']
+age = [20, 16, 52, 36, 60]
+address = ['서울', '강릉', '조선', '나주', '한성부']
+
+many_user=None
