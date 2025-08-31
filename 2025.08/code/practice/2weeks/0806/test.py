e6730257919e1f9fb2b381c41b7d4278e967c018
kim <kimms000311@naver.com>
2025-08-31 17:30:22 +0900

    remove : september 브랜치 이전에 따른 기존 root 디렉토리 삭제

--- 2025.08/code/practice/2weeks/0806/test.py
+++
@@ -1,8 +0,0 @@
-arr = [1, 2, 3]
-
-n = len(arr)  # 원소의 개수
-
-for i in range(1 << n):  # 1<<n: 부분 집합의 개수
-    for j in range(n):  # 원소의 수만큼 비트를 교환함
-        if i & (1 << j):  # i의j번쨰 비트가 1인경우
-            print(arr[j], end=',')  # j번 원소 출력
