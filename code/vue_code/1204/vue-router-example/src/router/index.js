f2dc0eadaebbe18b844c235c5848556bd5a1b768
--replace-all <kimms000311@naver.com>
2025-12-04 15:15:43 +0900

    feat : 1204 06-routing 실습 완료

---
+++ code/vue_code/1204/vue-router-example/src/router/index.js
@@ -0,0 +1,62 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import UserView from '@/views/UserView.vue'
+import UserProfile from '@/components/UserProfile.vue'
+import UserPosts from '@/components/UserPosts.vue'
+import LoginView from '@/views/LoginView.vue'
+
+
+const isAuthenticated = true
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/user/:id',
+      // name: 'user',
+      component: UserView,
+      children: [
+        { path: '', name: 'user', component: UserProfile },
+        { path: 'profile', name: 'userProfile', component: UserProfile },
+        { path: 'posts', name: 'userPosts', component: UserPosts }
+      ]
+    },
+    {
+      path: '/login',
+      component: LoginView,
+      name: 'login',
+      beforeEnter: (to, from) => {
+        if (isAuthenticated === true) {
+          console.log('이미 로그인 상태입니다')
+          return { name: 'home' }
+        }
+      }
+    }
+  ],
+})
+router.beforeEach((to, from) => {
+  console.log('to', to)
+  console.log('from', from)
+})
+
+// router.beforeEach((to, from) => {
+//   const isAuthenticated = false;
+
+//   if (!isAuthenticated && to.name !== 'login') {
+//     console.log('로그인 필요')
+//     return { name: 'login' }
+//   }
+// })
+export default router
---
+++ code/vue_code/1204/vue-router-example/src/views/UserView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <RouterLink :to="{ name: 'userProfile' }">Profile</RouterLink>
+        <RouterLink :to="{ name: 'userPosts' }">Posts</RouterLink>
+        <h3>UserView</h3>
+        <p>{{ userId }}번 유저</p>
+        <button @click="goHome">홈으로!</button>
+        <hr>
+        <RouterView />
+    </div>
+</template>
+
+<script setup>
+import { RouterView, useRoute, RouterLink, useRouter,onBeforeRouteLeave } from 'vue-router';
+import { ref } from 'vue';
+const route = useRoute()
+
+const router = useRouter()
+const goHome = () => {
+    router.push({ name: 'home' })
+    // router.replace({ name: 'home' })
+}
+const userId = ref(route.params.id)
+
+onBeforeRouteLeave((to,from)=>{
+    const answer = window.confirm('가지마요 ㅠㅠ')
+    if(answer === false){
+        return false
+    }
+})
+</script>
+
+<style scoped></style>
