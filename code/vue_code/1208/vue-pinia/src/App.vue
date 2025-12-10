fbb86df354b62b4fa3a33b9118572eafff26afea
--replace-all <kimms000311@naver.com>
2025-12-10 16:17:19 +0900

    feat : 1208 실습 커밋

---
+++ code/vue_code/1208/vue-pinia/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ store.count }}</h1>
+  <h3>{{ store.doubleCount }}</h3>
+  <button @click="store.increment">+++</button>
+</template>
+
+<script setup>
+import { useCounterStore } from './stores/counter';
+
+const store = useCounterStore()
+
+store.increment()
+console.log(store.count)
+console.log(store.doubleCount)
+  const newNumber = store.count + 1
+</script>
+
+<style scoped></style>
